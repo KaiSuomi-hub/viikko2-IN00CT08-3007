0239a5132c4d2f96ddd63215520751c7a8d4e771
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2024-09-09 18:47:21 +0300

    done modal

--- App.js
+++ App.js
@@ -1,52 +1,83 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
 
-const MyComponent = () => {
-    const [age, setAge] = useState("");
-    const [lower, setLower] = useState(null);
-    const [higher, setHigher] = useState(null);
-
-    const calculateLower = () => {
-        const result = (220 - parseFloat(age.replace(",", "."))) * 0.65;
-        setLower(result);
-    };
-
-    const calculateHigher = () => {
-        const result = (220 - parseFloat(age.replace(",", "."))) * 0.85;
-        setHigher(result);
-    };
-    // VV tällä ajetaan kaksi funktiota VV
-    const calculate = () => {
-        calculateLower();
-        calculateHigher();
-    };
-
+const App = () => {
+    const [modalVisible, setModalVisible] = useState(false);
     return (
-        <View style={styles.container}>
-            <Text style={styles.field}>Age</Text>
-            <TextInput
-                style={styles.field}
-                value={age}
-                onChangeText={(text) => setAge(text)}
-                keyboardType="decimal-pad"
-            />
-            <Text style={styles.field}>Limits</Text>
-            <Text style={styles.field}>
-                {lower !== null && higher !== null ? `${lower}-${higher}` : ""}
-            </Text>
-            <Button title="Calculate" onPress={calculate} />
+        <View style={styles.centeredView}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>This is modal</Text>
+                        <Pressable
+                            // style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={[styles.button, styles.buttonClose]}>Close</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Pressable
+                // style={[styles.button, styles.buttonOpen]}
+                onPress={() => setModalVisible(true)}
+            >
+                <Text style={styles.textStyle}>Show modal message</Text>
+            </Pressable>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    centeredView: {
         flex: 1,
-        marginTop: 50,
-        marginLeft: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
     },
-    field: {
-        marginBottom: 10,
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#dbdbdb",
+    },
+    textStyle: {
+        color: "black",
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
     },
 });
-export default MyComponent;
+
+export default App;
